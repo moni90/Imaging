8a99862eb0e09ca2d02b8d68b181a2c280b188f0
Hirofumi Nakayama <hnakayama4@gmail.com>
2020-03-23 13:09:17 -0400

    function to create roi masks from ImageJ

--- create_ROImask_manual.m
+++ create_ROImask_manual.m
@@ -3,41 +3,52 @@ function [roiMask_id,roiMask_stack]=create_ROImask_manual(name,opt)
 %ImageJ
 %Hirofumi Nakayama 2020
 
+%%
 %set up paths
-directory=configPath();
-
-if ~isfield(opt,'bmp')
-    opt.bmp = false;
+path_original = pwd;
+try
+    directory=configPath();
+    path_imaging = directory.home_2p;
+catch
+    path_imaging = pwd;
 end
 
-
-if isfield(opt,'path')
-    if contains(name,'RoiSet','IgnoreCase',true)
-        %If name of RoiSet is given
-        strROIArchiveFilename=fullfile(opt.path,sprintf('%s.zip',name));
-    else
-        %If name of tiff stack is given
-        strROIArchiveFilename=fullfile(opt.path,sprintf('%sRoiSet.zip',name));
-    end
-else
-    if contains(name,'RoiSet','IgnoreCase',true)
-        %If name of RoiSet is given
-        strROIArchiveFilename=fullfile(directory.home_2p,'Segmentation\RoiSet',sprintf('%s.zip',name));
-    else
-        %If name of tiff stack is given
-        strROIArchiveFilename=fullfile(directory.home_2p,'Segmentation\RoiSet',sprintf('%sRoiSet.zip',name));
-    end
-end
-
+%set parameters
+bmp = false;
 img_size = 512;
-if isfield(opt, 'img_size')
-    img_size = opt.img_size;
+strROIArchiveFilename = name;
+if exist('opt','var')
+    if isfield(opt,'bmp')
+        bmp = opt.bmp;
+    end
+    
+    if isfield(opt,'path')
+        if contains(name,'RoiSet','IgnoreCase',true)
+            %If name of RoiSet is given
+            strROIArchiveFilename=fullfile(opt.path,sprintf('%s.zip',name));
+        else
+            %If name of tiff stack is given
+            strROIArchiveFilename=fullfile(opt.path,sprintf('%sRoiSet.zip',name));
+        end
+    else
+        if contains(name,'RoiSet','IgnoreCase',true)
+            %If name of RoiSet is given
+            strROIArchiveFilename=fullfile(path_imaging,'Segmentation\RoiSet',sprintf('%s.zip',name));
+        else
+            %If name of tiff stack is given
+            strROIArchiveFilename=fullfile(path_imaging,'Segmentation\RoiSet',sprintf('%sRoiSet.zip',name));
+        end
+    end
+    
+    if isfield(opt, 'img_size')
+        img_size = opt.img_size;
+    end
 end
 
+%%
 %Read RoiSet.zip file
 %sROI{i}.vnRectBounds=[Top,Left,Bottom,Right]
 [sROI] = ReadImageJROI(strROIArchiveFilename);
-
 
 %Create binary masks for individual roi
 [cc,rr] = meshgrid(1:img_size);
@@ -69,6 +80,7 @@ for i=1:numel(sROI)
         c=mean(sROI{i}.vnRectBounds([2,4]));
         rw=(sROI{i}.vnRectBounds(3)-sROI{i}.vnRectBounds(1))/2;
         cw=(sROI{i}.vnRectBounds(4)-sROI{i}.vnRectBounds(2))/2;
+        centOfMass(i,:) = [r, c];
         
         %Create ellipse masks from bounding box
         roiMasks(:,:,i) = sqrt(((rr-r)/rw).^2+((cc-c)/cw).^2)<1;
@@ -94,7 +106,7 @@ roiMask_id=zeros(size(roiMasks(:,:,1)));
 
 %rank order center of mass in ascending order
 [~,p] = sort(centOfMass(:,1),'ascend');
-posRank = 1:length(Data);
+posRank = 1:length(p);
 posRank(p) = posRank;
 
 %Create single images containing id of roiMasks
@@ -110,16 +122,14 @@ for i=1:numel(sROI)
     roiMask_stack(:,:,i)=logical(roiMask_id==i);
 end
 
-cd(fullfile(directory.home_2p,'Segmentation'))
-save(sprintf('%sroiMask_Manual',name),'roiMask_id','roiMask_stack');
-cd(fullfile(directory.home_2p))
-
 %Create .bmp files for individual ROIs
-if opt.bmp
-    new_folder = fullfile(directory.home_2p,'Segmentation','bmp');
+if bmp
+    new_folder = fullfile(path_imaging,'Segmentation','bmp');
     mkdir(new_folder)
     cd(new_folder)
     for i = 1:size(roiMask_stack,3)
         imwrite(1-roiMask_stack(:,:,i),sprintf('%s_roi_bmp_%d.bmp',name,i),'bmp')
     end
 end
+
+cd(path_original)
